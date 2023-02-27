5513dbfa1974873164e2c21a68280b2a5ce531e0
Jakob Thrane <jath@unumed.com>
2023-02-27 07:30:55 +0100

    Font added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
+import brandLinkedin from './assets/brand-linkedin.svg';
+import brandInstagram from './assets/brand-instagram.svg';
 import './App.css';
 
 const Header = () => {
   return (
     <div className="lg:py-100 sticky top-0 z-50 px-16 py-16 lg:px-40">
-      <div className="flex-wrap items-center justify-between sm:space-y-10 lg:flex">
-        <div className="text-5xl font-bold">Sannes</div>
-        <div className="flex  space-x-16 lg:justify-evenly">
-          <div>About</div>
-          <div>Concepts</div>
-          <div>Blog</div>
+      <div className="flex-wrap justify-between sm:space-y-10 lg:flex lg:items-end">
+        <div className="text-4xl font-bold">sannethrane.com</div>
+        <div className="flex space-x-16 lg:justify-evenly">
+          <div>work</div>
+          <div>about</div>
+          <img src={brandLinkedin} />
+          <img src={brandInstagram} />
         </div>
+      </div>
+    </div>
+  );
+};
+
+const Landing = () => {
+  return (
+    <div className="mb-100 mx-40 h-full">
+      <div className="flex flex-row ">
+        <div className="flex w-1/4 flex-initial">
+          <div className="h-20">
+            Designer with passion for the ocean, susstainability &
+            responsebility
+          </div>
+        </div>
+        <div className="flex-auto"> image</div>
       </div>
     </div>
   );
@@ -20,8 +39,10 @@ const Header = () => {
 function App() {
   return (
     <div className="App ">
-      <Header />
-      <div className="relative h-screen bg-gradient-to-r from-cyan-500 to-blue-500"></div>
+      <div className="flex h-screen flex-col">
+        <Header />
+        <Landing />
+      </div>
     </div>
   );
 }
